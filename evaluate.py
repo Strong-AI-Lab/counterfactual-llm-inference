d8ee839de8266e5c9745ab5efec277e4d576f778
ggendro <g.gendron35@gmail.com>
2024-09-28 21:08:37 +0000

    added cladder evaluation

---
+++ evaluate.py
@@ -0,0 +1,260 @@
+
+import os
+import yaml
+import time
+import argparse
+import random
+from typing import List, Union, Dict, Any, Union, Optional, Callable
+import tqdm
+import networkx as nx
+
+from src.data.evaluation_dataset import EVALUATION_DATASETS 
+from src.model.models import BUILDERS, MERGERS, QUERY_INTERPRETERS, INFERENCE_ORACLES, EVALUATORS
+from src.causal.counterfactual_inference import Query
+from src.model.singleton import handle_config_singleton
+from src.visualisation.visualisation import save_graph_as_png
+
+from json.decoder import JSONDecodeError
+from langchain_core.exceptions import OutputParserException
+from networkx.exception import NetworkXError, NetworkXUnfeasible
+
+
+DEFAULT_EVALUATION_GRAPHS_SAVE_FOLDER = 'evaluation_logs'
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Compute end-to-end causal discovery and inference on dataset with ground truth')
+    parser.add_argument('config', type=str, help='Path to the query configuration file')
+    return parser.parse_args()
+
+
+def build_query(graph : nx.DiGraph, query_text : str, interpreter : Callable) -> Dict[str,Any]:
+    nodes = list(graph.nodes(data=True))
+    query = interpreter(query_text, nodes)
+
+    return {'target_node': query['target_variable'],
+            'observation_nodes': None, # no observations
+            'intervention_nodes': query['intervention_variable'],
+            'observation_values': None, # no observations
+            'intervention_values': query['intervention_new_value'],
+            }
+
+def build_topological_graph(graph : nx.DiGraph) -> nx.DiGraph:
+    order = list(nx.topological_sort(graph))
+    node_relabel = {node: str(i) for i, node in enumerate(order)}
+    return nx.relabel_nodes(graph, node_relabel, copy=True)
+
+def sem_equals(a : str, b :str) -> bool:
+    positives = ['yes', 'true', '1', 'y', 't']
+    negatives = ['no', 'false', '0', 'n', 'f']
+
+    a_pos = a.lower() in positives
+    b_pos = b.lower() in positives
+
+    a_neg = a.lower() in negatives
+    b_neg = b.lower() in negatives
+
+    if not (a_pos or a_neg) or not (b_pos or b_neg):
+        return a == b
+    else:
+        return (a_pos and b_pos) or (a_neg and b_neg)
+
+def match_attributes(graph : nx.DiGraph, ref_graph : nx.DiGraph):
+    graph = graph.copy()
+
+    node_relabel = {}
+    for node in graph.nodes:
+        graph.nodes[node]['observed'] = True
+        for ref_node in ref_graph.nodes:
+            if node.lower() == ref_graph.nodes[ref_node]['description'].lower():
+                graph.nodes[node]['description'] = ref_graph.nodes[ref_node]['description']
+                node_relabel[node] = ref_node
+                break
+    
+    nx.relabel_nodes(graph, node_relabel, copy=False)
+    
+    return graph
+
+def save_errors(output_path : str, iteration_name : str, error : str):
+    output_path_i = os.path.join(output_path, f'iteration_{iteration_name}')
+    os.makedirs(output_path_i, exist_ok=True)
+
+    with open(os.path.join(output_path_i, 'errors.txt'), 'w') as f:
+        f.write(error)
+
+def main(data_path : Union[str,List[str]], 
+         dataset_class : str, 
+         dataset_config : Dict[str,Any],
+         evaluator_class : str, 
+         evaluator_config : Dict[str,Any],
+         oracle_class : str, 
+         oracle_config : Dict[str,Any],
+         interpreter_class : str, 
+         interpreter_config : Dict[str,Any], 
+         builder_class : str, 
+         builder_config : Dict[str,Any], 
+         merger_class : Optional[str] = None, 
+         merger_config : Optional[Dict[str,Any]] = None,
+         graph_traversal_cutoff : Optional[int] = None,
+         save_graphs : bool = False,
+         save_logs : bool = True,
+         nb_queries : Optional[int] = None, # for debugging purposes
+         output_path : Optional[str] = None):
+    
+    # Load text data
+    print(f"Using dataset {dataset_class}.")
+    data = EVALUATION_DATASETS[dataset_class](data_path, **dataset_config)
+    
+    if nb_queries is not None:
+        random.seed(42)
+        indices = random.sample(range(len(data)), nb_queries)
+        data = data[indices]
+
+    # Load graph builder: build causal graphs from text data
+    print(f"Using builder {builder_class}.")
+    builder = BUILDERS[builder_class](**builder_config)
+
+    # Load oracle: answer counterfactual queries
+    print(f"Using oracle {oracle_class}.")
+    oracle = INFERENCE_ORACLES[oracle_class](**oracle_config)
+
+    # Load interpreter: interpret a node to build alternative/counterfactual instantiations
+    print(f"Using interpreter {interpreter_class}.")
+    interpreter = QUERY_INTERPRETERS[interpreter_class](**interpreter_config)
+
+    # Load evaluator: evaluate the plausibility of counterfactual graphs
+    print(f"Using evaluator {evaluator_class}.")
+    evaluator = EVALUATORS[evaluator_class](**evaluator_config)
+
+
+    if output_path is None:
+        output_path = os.path.join(DEFAULT_EVALUATION_GRAPHS_SAVE_FOLDER, f'logs_evaluation_{dataset_class}_{time.strftime("%Y%m%d-%H%M%S")}')
+    os.makedirs(output_path, exist_ok=True)
+
+
+    # Iteratively build causal graphs
+    scores = []
+    for item in tqdm.tqdm(data):
+        name = item['name']
+        text = item['text']
+        query_text = item['query']
+        gt_graph = item['gt_graph']
+        label = item['label']
+        
+        # Build graph
+        try:
+            graph = builder.build_graph(name, text)
+        except (KeyError, JSONDecodeError, OutputParserException, NetworkXError) as e:
+            print(f"Error building graph: {e}")
+            save_errors(output_path, name, f"Error building graph: {e}")
+            continue
+
+        # Answer counterfactual queries (num_queries per graph)
+        query_config = build_query(graph, query_text, interpreter)
+
+        # Compute counterfactuals
+        try:
+            query = Query(graph, oracle, **query_config, traversal_cutoff=graph_traversal_cutoff, compute_counterfactuals=True)
+            answer, computation_graph = query()
+        except (KeyError, JSONDecodeError, OutputParserException) as e:
+            print(f"Error computing counterfactuals: {e}")
+            save_errors(output_path, name, f"Error computing counterfactuals: {e}")
+            continue
+
+        # Evaluate counterfactual graphs
+        score = {
+            'item' : item,
+        }
+
+        # Compute label accuracy
+        correct = sem_equals(answer, label)
+        score['correct'] = correct
+        score['answer'] = answer
+        score['label'] = label
+
+        # Update ground truth graph
+        gt_graph = match_attributes(gt_graph, graph)
+
+        # Compute graph self-scores
+        # for g, name in [(gt_graph, "ground_truth"), (graph, "estimated"), (computation_graph, "counterfactual")]:
+        for g, g_name in [(graph, "estimated"), (computation_graph, "counterfactual")]:
+            try:
+                plausibility, confidence, explanation = evaluator.evaluate(g)
+            except (JSONDecodeError, OutputParserException, NetworkXUnfeasible) as e:
+                print(f"Error evaluating graph: {e}")
+                save_errors(output_path, name, f"Error evaluating graph: {e}")
+                plausibility, confidence, explanation = None, None, None
+
+            g.graph['plausibility_score'] = plausibility
+            g.graph['plausibility_score_confidence'] = confidence
+            g.graph['plausibility_score_explanation'] = explanation
+
+            score[f'{g_name}_plausibility'] = plausibility
+            score[f'{g_name}_confidence'] = confidence
+            score[f'{g_name}_explanation'] = explanation
+
+        # Compute graph edit distance (ged) similarity
+        ged = nx.graph_edit_distance(gt_graph, graph)
+        score['graph_edit_distance'] = ged
+
+        # Compute intersection over union similarity
+        intersection = nx.intersection(gt_graph, graph)
+        union = nx.compose(gt_graph, graph)
+        iou_ged = nx.graph_edit_distance(intersection, union)
+        score['intersection_over_union_graph_edit_distance'] = iou_ged
+
+        # Compute label-free ged
+        if nx.is_directed_acyclic_graph(gt_graph) and nx.is_directed_acyclic_graph(graph):
+            gt_graph_topo = build_topological_graph(gt_graph)
+            graph_topo = build_topological_graph(graph)
+            ged_topo = nx.graph_edit_distance(gt_graph_topo, graph_topo)
+
+            # Compute label-free iou_ged
+            intersection_topo = nx.intersection(gt_graph_topo, graph_topo)
+            union_topo = nx.compose(gt_graph_topo, graph_topo)
+            iou_ged_topo = nx.graph_edit_distance(intersection_topo, union_topo)
+        else:
+            save_errors(output_path, name, f"Error computing topological graph: graphs are not DAGs\ngt_graph: {nx.is_directed_acyclic_graph(gt_graph)}\ngraph: {nx.is_directed_acyclic_graph(graph)}")
+            ged_topo = None
+            iou_ged_topo = None
+        
+        score['graph_edit_distance_topological'] = ged_topo
+        score['intersection_over_union_graph_edit_distance_topological'] = iou_ged_topo
+
+        scores.append(score)
+
+        if save_logs or save_graphs:
+            output_path_i = os.path.join(output_path, f'iteration_{name}')
+            os.makedirs(output_path_i, exist_ok=True)
+        
+        if save_logs:
+            # Write logs
+            with open(os.path.join(output_path_i, 'logs.txt'), 'w') as f:
+                for key, value in score.items():
+                    f.write(f"{key}: {value}\n")
+
+        if save_graphs:
+            # Write graph
+            nx.write_gml(gt_graph, os.path.join(output_path_i, f'ground_truth_graph.gml'))
+            save_graph_as_png(gt_graph, os.path.join(output_path_i, f'ground_truth_graph.png'), node_labels=[], edge_labels=[])
+
+            nx.write_gml(graph, os.path.join(output_path_i, f'initial_graph_score={score["estimated_plausibility"]}.gml'))
+            save_graph_as_png(graph, os.path.join(output_path_i, f'initial_graph_score={score["estimated_plausibility"]}.png'), node_labels=['description', 'type', 'values', 'current_value', 'context'], edge_labels=['description', 'details'])
+
+            nx.write_gml(computation_graph, os.path.join(output_path_i, f'counterfactual_graph_score={score["counterfactual_plausibility"]}.gml'))
+            save_graph_as_png(computation_graph, os.path.join(output_path_i, f'counterfactual_graph_score={score["counterfactual_plausibility"]}.png'), node_labels=['description','current_value','updated_value'])
+        
+
+
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    config_path = args.config
+    
+    with open(config_path, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    config = handle_config_singleton(config)
+    main(**config)
---
+++ src/data/evaluation_dataset.py
@@ -0,0 +1,96 @@
+
+import abc
+import re
+from typing import Dict, Any, Tuple, List, Union, Optional
+import networkx as nx
+
+import datasets
+
+
+class EvaluationDataset(abc.ABC):
+    
+    def __init__(self, path : Optional[Union[str,List[str]]] = None) -> None:
+        pass
+
+    @abc.abstractmethod
+    def __len__(self) -> int:
+        pass
+    
+    @abc.abstractmethod
+    def __getitem__(self, idx) -> str:
+        pass
+    
+    @abc.abstractmethod
+    def __iter__(self):
+        pass
+
+
+class CladderDataset(EvaluationDataset):
+
+    def _parse_text_graph(self, text : str) -> nx.DiGraph:
+        graph = nx.DiGraph()
+        text_without_instructions = re.match(r'.*?: (.*)', text).group(1)
+        lines = text_without_instructions.split('. ')
+
+        graph = nx.DiGraph()
+        edge_reg = re.compile(r'(.*) has a direct effect on (.*)')
+        for line in lines[:-1]:
+            edge_match = edge_reg.match(line)
+            if edge_match:
+                cause = edge_match.group(1)
+                effects = edge_match.group(2).split(' and ')
+
+                cause = cause.lower()
+                effects = [effect.lower() for effect in effects]
+
+                if not graph.has_node(cause):
+                    graph.add_node(cause)
+                for effect in effects:
+                    if not graph.has_node(effect):
+                        graph.add_node(effect)
+                    graph.add_edge(cause, effect)
+
+        return graph
+
+    def _parse_item(self, idx : int, item : Dict[str,Any]) -> Dict[str,Any]:
+        result_item = {}
+        result_item['name'] = str(item['id'])
+        result_item['label'] = item['label']
+
+        split_text = item['prompt'].split('. ')
+        query = split_text[-1]
+        text = '. '.join(split_text[:-1])
+
+        result_item['text'] = text
+        result_item['query'] = query
+
+        gt_graph = self._parse_text_graph(item['prompt'])
+        result_item['gt_graph'] = gt_graph
+
+        return result_item
+    
+    def __init__(self, path : Optional[Union[str,List[str]]] = None) -> None:
+        data = datasets.load_dataset('causal-nlp/Cladder')['full_v1.5_default']
+        self.data = data.filter(lambda e : e['query_type'] == 'det-counterfactual')
+    
+    def __len__(self) -> int:
+        return len(self.data)
+    
+    def __getitem__(self, idx : int | List[int]) -> Dict[str,Any]:
+        if isinstance(idx, list):
+            return [self._parse_item(i, self.data[i]) for i in idx]
+        elif isinstance(idx, slice):
+            return [self._parse_item(i, self.data[i]) for i in range(*idx.indices(len(self)))]
+        else:
+            return self._parse_item(idx, self.data[idx])
+    
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+    
+
+
+
+EVALUATION_DATASETS = {
+    'cladder' : CladderDataset
+}
